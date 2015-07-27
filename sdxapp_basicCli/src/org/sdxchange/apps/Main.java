516182cba97044e237d0f7f6f9f1025de1be3f48
ivan-perl <ammorwelle@mail.ru>
2015-07-27 19:13:45 +0300

    Working on sources refactoring.  - Added module for xmile plugin  - Added placeholder for console interface for the project

---
+++ sdxapp_basicCli/src/org/sdxchange/apps/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ivan on 27.07.2015.
+ */
+
+package org.sdxchange.apps;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
---
+++ sdxmod_xmile/src/org/sdxchange/lang/xmile/plugin/PluginIdentity.java
@@ -0,0 +1,7 @@
+package org.sdxchange.lang.xmile.plugin;
+
+/**
+ * Created by Ivan on 24.07.2015.
+ */
+public class PluginIdentity implements org.sdxchange.plugin.api.SDXBidirectionalPlugin {
+}
